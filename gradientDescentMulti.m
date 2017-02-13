94db8355184c2a41ab239146552b994edb8d66e9
Yosua Tresno Surojo <yosua1011@gmail.com>
2017-02-13 10:58:15 +0700

    Gradient Descent Multi Variables
    Modified the theta and cost function formula to suit with multi variables gradient descent calculation

--- gradientDescentMulti.m
+++ gradientDescentMulti.m
@@ -5,6 +5,7 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = size(X, 2);
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
@@ -17,15 +18,13 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
-
-
-
-
-
+    h = X * theta;
+	B = zeros(n , 1);
+	for i = 1 : m,
+		B = B + (h(i) - y(i)) * X(i,:)';	
+	end
+	
+	theta = theta - (alpha * B) / m;
 
     % ============================================================
 
